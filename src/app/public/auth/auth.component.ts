d3801b6c7e13727e5c9393144be1b10697d8255c
Alan Allison <alan@veevra.com>
2017-01-05 14:48:46 -0800

    Bug fix: Email/username was not getting referenced correctly on confirm registration page

--- src/app/public/auth/auth.component.ts
+++ src/app/public/auth/auth.component.ts
@@ -96,7 +96,7 @@ export class RegistrationConfirmationComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
-            this.email = params['email'];
+            this.email = params.username;
 
         });
 
