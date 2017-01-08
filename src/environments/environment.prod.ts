e76d2ae9b7bbc7de6dabb007548db651aef87115
Vladimir Budilov <Vladimir Budilov>
2017-01-07 21:10:04 -0500

    added the appropriate prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-    production: true
+    production: true,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
+    userPoolId: 'us-east-1_PGSbCVZ7S',
+    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail'
 };
