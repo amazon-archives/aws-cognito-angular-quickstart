72512e6365ab2bebcad8256d0fa605fdf01db8de
Vladimir Budilov <Vladimir Budilov>
2017-01-07 19:12:15 -0500

    Cleaned up the files and reorged them

--- src/app/service/properties.service.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by vova on 11/23/16.
- */
-
-export let _PHOTOS_SERVICE_URL: string = ""
-export let _IDENTITY_POOL_ID: string = ""
-export let _USER_POOL_ID: string = ""
-export let _REKOGNITION_BUCKET: string = "rekognition-pics"
