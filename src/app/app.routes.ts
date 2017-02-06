ca6a595a22a60c52e0aae5b606fc516aa72c838b
Vladimir Budilov <odessitv@gmail.com>
2017-02-06 00:47:26 -0500

    added the photos routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {MyProfileComponent} from "./secure/profile/myprofile.component";
 import {JwtComponent} from "./secure/jwttokens/jwt.component";
 import {UseractivityComponent} from "./secure/useractivity/useractivity.component";
 import {AppComponent} from "./app.component";
+import {PhotosComponent} from "./secure/photos/photos.component";
 import {LoginComponent} from "./public/auth/login/login.component";
 import {RegisterComponent} from "./public/auth/register/registration.component";
 import {ForgotPassword2Component, ForgotPasswordStep1Component} from "./public/auth/forgot/forgotPassword.component";
@@ -47,7 +48,8 @@ const secureHomeRoutes: Routes = [
         {path: 'jwttokens', component: JwtComponent},
         {path: 'myprofile', component: MyProfileComponent},
         {path: 'useractivity', component: UseractivityComponent},
-        {path: '', component: MyProfileComponent}]
+        {path: 'photos', component: PhotosComponent},
+        {path: '', component: PhotosComponent}]
     }
 ];
 
